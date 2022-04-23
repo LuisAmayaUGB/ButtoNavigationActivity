8f779334ae13d173182e78636b403acbf76f0c81
luis antonio amaya villalta <amayantonio@hotmail.com>
2022-04-23 16:52:03 -0600

    Button Navigation Activity

---
+++ app/src/main/java/com/example/myapplicationbo/ui/home/HomeFragment.kt
@@ -0,0 +1,51 @@
+package com.example.myapplicationbo.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplicationbo.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val eltexto=binding.txtTexto
+        val elboton=binding.btnBoton
+
+        elboton.setOnClickListener {
+            eltexto.text="Bienvenidos!"
+        }
+
+
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
